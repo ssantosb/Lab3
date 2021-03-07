eb30d9059c832d81e8551e4e7de996fa870c8a75
Nico Urrego <n.urrego@uniandes.edu.co>
2021-03-07 10:58:58 -0500

    Creación del proyecto

---
+++ src/server/Server.java
@@ -0,0 +1,46 @@
+package server;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Server {
+	
+	public final static int PUERTO = 21; //Se utiliza FTP
+	public final static String ARCHIVO100 = "data/Archivo100mb";
+	public final static String ARCHIVO250 = "data/Archivo250mb";
+	public final static String DIR_LOGS = "logs/";
+	public final static int BUFFER = 64000;
+	public final static int MIN_CON = 25;
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String tiempo = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime());
+		File log = new File(DIR_LOGS + tiempo + ".txt");
+		BufferedWriter bw;
+		ServerSocket socket;
+		try
+		{
+			bw = new BufferedWriter(new FileWriter(log));
+			bw.write("Fecha inicio: " + tiempo);
+			bw.newLine();
+			bw.flush();
+			
+			
+		}
+		catch(IOException io)
+		{
+			//error escribiendo buffer
+		}
+		catch(Exception e)
+		{
+			
+		}
+	}
+
+}
